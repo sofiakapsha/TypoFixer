3a6b44de5d6182cc5ca362a13e7917ea07360afd
sofiiakapsha <sofiia.kapsha.25@kse.org.ua>
2026-04-13 21:19:06 +0300

    added DictionaryLoader.cs to load words Dictionary.

---
+++ TypoFixer/TypoFixer/DictionaryLoader.cs
@@ -0,0 +1,29 @@
+﻿namespace TypoFixer;
+
+public class DictionaryLoader
+{
+    public static HashSet<string> LoadDictionary(string line)
+    {
+        var words = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        try
+        {
+            foreach (var word in File.ReadLines(line))
+            {
+                string clearedWord = word.Trim();
+
+                if (!string.IsNullOrWhiteSpace(clearedWord))
+                {
+                    words.Add(clearedWord);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error during reding the file: {e}");
+        }
+
+        return words;
+
+    }
+}
