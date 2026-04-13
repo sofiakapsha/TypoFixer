8d56d0bf881c74057459ab72261275a60ab63515
sofiiakapsha <sofiia.kapsha.25@kse.org.ua>
2026-04-13 23:14:39 +0300

    added LCS realization

---
+++ TypoFixer/TypoFixer/LCSLengths.cs
@@ -0,0 +1,29 @@
+﻿namespace TypoFixer;
+
+public static class LCSLengths
+{
+    public static int ComputeLCS(string user, string dictionary)
+    {
+        int m = user.Length;
+        int n = dictionary.Length;
+        
+        int[,] common = new int[m + 1, n + 1];
+
+        for (var i = 1; i <= m; i++)
+        {
+            for (var j = 1; j <= n; j++)
+            {
+                if (user[i - 1] == dictionary[j - 1])
+                {
+                    common[i, j] = common[i - 1, j - 1] + 1;
+                }
+                else
+                {
+                    common[i, j] = Math.Max(common[i, j - 1], common[i - 1, j]);
+                }
+            }
+        }
+
+        return common[m, n];
+    }
+}
