2b6d9c3c7cd6bb502e11dc6c18cc3daf300e65fd
sofiiakapsha <sofiia.kapsha.25@kse.org.ua>
2026-04-14 09:55:39 +0300

    added words_list and changed Program.cs to run test the work of algorithms

--- TypoFixer/TypoFixer/Program.cs
+++ TypoFixer/TypoFixer/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello world!");
+﻿using TypoFixer;
+
+var dictionary = DictionaryLoader.LoadDictionary("words_list.txt");
+Console.WriteLine(Levenshtein.ComputeLevenshtein("lifer", "life")); // Має бути 1
+Console.WriteLine("Write your sentence: ");
+var input = Console.ReadLine();
+
+string[] inputWords = input.Split(new[] { ' ', ',', '.', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+var suggestor = new Suggestor();
+
+foreach (var word in inputWords)
+{
+    if (!dictionary.Contains(word))
+    {
+        Console.WriteLine($"\nFound the mistake: '{word}'");
+        
+        var hints = suggestor.GetSuggestions(word, dictionary);
+        
+        Console.WriteLine("Maybe you meant: " + string.Join(", ", hints));
+    }
+}
